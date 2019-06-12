ae9e0f93b5dc8e9d88b6e899197dbfeac8c81551
femtioelva <nejfredrika@gmail.com>
2019-06-12 12:38:47 +0200

    add initial js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,31 @@
-alert('hello!');
+$(function() {
+    $('.resultGroup').hide();
+})
+
+let addOption = function(){
+    $('.inputs').append('<input type="text" placeholder="Something else">');
+}
+
+let changeTitleColor = function() {
+
+};
+
+$('.addButton').click(addOption);
+
+
+$('.apple').click(function(){
+    $(this).addClass("rotate-center");
+});
+
+$('.apple').hover(function(){
+    $(this).removeClass("rotate-center");
+});
+
+/*
+Spinn on hover
+
+$('.apple').hover(function(){
+    $(this).toggleClass("rotate-center");
+});
+
+*/
