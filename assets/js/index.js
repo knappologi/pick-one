dc7772464b6cb6085fb4e4679dc0902e81036dad
femtioelva <nejfredrika@gmail.com>
2019-06-13 12:54:38 +0200

    finish assignment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,38 @@
+const states = {
+    START: 'start',                 // When entering the page, or when reseting all options
+    PICKONEFORME: 'pickoneforme',    // Pick a winner first time
+    NEWPICK: 'newpick',             // Pick another from the options 
+
+}
+let options = [];
+let state = states.START;
+
+
 $(function() {
-    $('.resultGroup').hide();
+    $('.resultGroup').none();
 })
 
 let addOption = function(){
     $('.inputs').append('<input type="text" placeholder="Something else">');
 }
-
-let changeTitleColor = function() {
-
-};
-
 $('.addButton').click(addOption);
+
+
+let submit = function(){
+    $('.inputGroup').hide();
+    
+    let winner;
+    $('input').each(function(){
+        options.push($(this).val());
+    })
+    
+    winner = options[Math.floor(Math.random() * options.length)];
+    console.log(winner);
+    $('.resultGroup').append('<h2>' + winner + '</h2>');
+    $('.resultGroup').display();
+}
+$('.submit').click(submit);
+
 
 
 $('.apple').click(function(){
@@ -20,6 +42,8 @@ $('.apple').click(function(){
 $('.apple').hover(function(){
     $(this).removeClass("rotate-center");
 });
+
+
 
 /*
 Spinn on hover
