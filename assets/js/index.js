eee9bd7b9c16d9b66ec945a4cfedf82844a17dc9
femtioelva <nejfredrika@gmail.com>
2019-06-13 14:04:39 +0200

    added states

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,48 @@
 const states = {
     START: 'start',                 // When entering the page, or when reseting all options
     PICKONEFORME: 'pickoneforme',    // Pick a winner first time
-    NEWPICK: 'newpick',             // Pick another from the options 
+    ANOTHERPICK: 'newpick',             // Pick another item from the options 
 
 }
 let options = [];
 let state = states.START;
+let winner = '';
 
 
 $(function() {
-    $('.resultGroup').none();
+    $('.resultGroup').hide();
 })
 
 let addOption = function(){
-    $('.inputs').append('<input type="text" placeholder="Something else">');
+    $('.inputs').append('<input type="text" class="addedOption" placeholder="Something else">');
 }
 $('.addButton').click(addOption);
 
 
 let submit = function(){
-    $('.inputGroup').hide();
-    
-    let winner;
+    if (state = states.PICKONEFORME) {
+        winner = '';
+    }
+ 
     $('input').each(function(){
-        options.push($(this).val());
+        let addedItem = $(this).val();
+        if (addedItem != '') {
+        options.push(addedItem);
+        }
+        console.log('this is options' + options)
     })
+    console.log(options.length);
     
+    if(options.length != 0) {
+    while(winner == '') {
     winner = options[Math.floor(Math.random() * options.length)];
-    console.log(winner);
-    $('.resultGroup').append('<h2>' + winner + '</h2>');
-    $('.resultGroup').display();
+    }
+    console.log('winner is: ' + winner);
+    $('.resultGroup').show();
+    state = states.PICKONEFORME;
+    switchState();
+ //   winner = '';
+}
 }
 $('.submit').click(submit);
 
@@ -43,6 +56,37 @@ $('.apple').hover(function(){
     $(this).removeClass("rotate-center");
 });
 
+let setPickoneForMe = function(){
+    $('.pickedItem').remove();
+    $('.resultGroup').append('<h2 class="pickedItem">' + winner + '</h2>');
+    $('.inputGroup').hide();
+    $('.resultGroup').show();
+}
+
+let switchState = function(){
+    switch(state) {
+        case states.START:
+            setStartState();
+            
+            //stuff
+            break;
+        case states.PICKONEFORME:
+            setPickoneForMe();
+            //stuff
+            break;
+        case states.ANOTHERPICK:
+    }
+}
+
+let setStartState = function(){
+    options.length = 0;     // clear options array 
+    $('input:text').val('');
+    $('.inputGroup').show();
+    $('.resultGroup').hide();
+    $('.addedOption').remove();
+}
+
+$('.clearButton').click(setStartState);
 
 
 /*
